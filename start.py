b74235b21099efca77ce6f4913b9eb00d52ff0db
Константин Зейкин <kostia.zeykin@gmail.com>
2026-04-10 09:25:04 +0300

    delete comments from start.py

--- start.py
+++ start.py
@@ -3,14 +3,9 @@ from classes import *
 import sys
 
 
-''' Start main program '''
 if __name__ == '__main__':
-    # Create application app.
     app = QApplication(sys.argv)
-    # Create main window for all other widgets.
     main_window = MainWindow()
     main_window.setFont(QFont('Font/pfdintextpro-thinitalic.ttf', 10, 30, False))
-    # Showing the main windows
     main_window.show()
-    # Start app with method exec_, which starting loop
     sys.exit(app.exec())
